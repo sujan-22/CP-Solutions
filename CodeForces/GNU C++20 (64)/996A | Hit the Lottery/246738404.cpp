99ee20f3f7186bb0d453f3ac04cc315cd2a58b93
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-02-19 02:14:54 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/996A | Hit the Lottery/246738404.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    long long n;
+    cin >> n;
+    int bills[] = { 100, 20, 10, 5, 1 };
+    int count = 0;
+ 
+    for (int i = 0; i < 5; ++i) {
+        count += n / bills[i];
+        n %= bills[i];
+    }
+    cout << count << endl;
+    return 0;
+}
