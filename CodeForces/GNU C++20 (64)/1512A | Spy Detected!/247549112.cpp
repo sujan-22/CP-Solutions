0e0115eb66d1db92b18a18fb03f6adc3afc5ba5a
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-02-20 19:33:52 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1512A | Spy Detected!/247549112.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <list>
+#include <unordered_map>
+#define ll long long
+#define FOR(p,n) for (int i = p; i < n; ++i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define vec(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define set(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+ 
+using namespace std;
+ 
+int t;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin>>t;
+ tc {
+     int n;
+    cin>>n;
+    umap(int,int) freq;
+    vector<int> a(n);
+ 
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+            freq[a[i]]++;
+        }
+        for (int i = 0; i < n; ++i) {
+            if (freq[a[i]] == 1) {
+                cout << i + 1 << endl;
+                break;
+            }
+        }
+ }           
+    return 0;
+}
+ 
