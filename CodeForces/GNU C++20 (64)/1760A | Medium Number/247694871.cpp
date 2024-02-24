610e6f63f276d550fb87bdfb0cba3de1c3867698
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-02-23 21:52:28 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1760A | Medium Number/247694871.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <list>
+#include <unordered_map>
+#define ll long long
+#define FOR(p,n) for (int i = p; i < n; ++i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define vec(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+ 
+using namespace std;
+ 
+int main() {
+   int t;
+   cin>>t;
+   tc{
+        vec(int) a(3) ;
+        FOR(0,3){
+            cin>>a[i];
+        }    
+        sort(all(a));
+        cout<<a[1]<<'\n';
+   }
+ 
+    return 0;
+}
