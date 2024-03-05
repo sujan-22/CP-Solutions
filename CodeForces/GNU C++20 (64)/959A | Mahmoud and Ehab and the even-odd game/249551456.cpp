b5bb4370d46336c3eb37be36547f10704e6b9f5d
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-03-05 01:39:29 -0500

    Committing files

---
+++ CodeForces/GNU C++20 (64)/959A | Mahmoud and Ehab and the even-odd game/249551456.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define vec(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define vecIn(a) for(int i=0;i<n;++i){cin>>a[i];}
+ 
+using namespace std;
+ 
+void solve(){
+    ll n;
+    cin>>n;
+    n&1?cout<<"Ehab\n":cout<<"Mahmoud\n";
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    //cin>>t;
+    tc{
+        solve();
+    }
+ 
+    return 0;
+}
+ 
