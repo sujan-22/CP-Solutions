7ca9080a116c17acc009e6bff18c2d382b963c90
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-03-07 22:40:16 -0500

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1154A | Restoring Three Numbers/247162617.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+#define FOR(i,n) for (int i = 0; i < n; ++i)
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    vector<int> arr(4);
+    FOR(i,4){
+        cin >> arr[i];
+    }
+    
+    sort(arr.begin(), arr.end());
+    FOR(i, 3){
+        cout << arr[3] - arr[i] << " ";
+    }
+ 
+    return 0;
+}
