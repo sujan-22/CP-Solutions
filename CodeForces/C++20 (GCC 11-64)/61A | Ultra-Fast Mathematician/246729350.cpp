6931963a0f217147c7343bf5040710ce38d41464
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-03-07 22:41:15 -0500

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/61A | Ultra-Fast Mathematician/246729350.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    string num1, num2;
+    cin >> num1 >> num2;
+ 
+    string result;
+    for (int i = 0; i < num1.size(); ++i) {
+        if (num1[i] != num2[i]) {
+            result += '1';
+        }
+        else {
+            result += '0';
+        }
+    }
+ 
+    cout << result << endl;
+    return 0;
+}
