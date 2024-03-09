6822c3751d67cb2410ef7ec4f85081174531fb6a
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-03-09 02:37:57 -0500

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1560B | Who's Opposite?/250300860.cpp
@@ -0,0 +1,52 @@
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define rev(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define vec(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+ 
+using namespace std;
+ 
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int diff = abs(a-b);
+    int total = 2*diff;
+    if(a>total||b>total||c>total){
+        cout<<"-1\n";
+        return;
+    }
+    int d = c + diff;
+    if(d>total){
+        d = c - diff;
+    }
+    cout<<d<<'\n';
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t;
+    cin>>t;
+    tc{
+        solve();
+    }
+ 
+    return 0;
+}
+ 
