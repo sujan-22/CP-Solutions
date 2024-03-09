c2eec3321bc3d42924aee32c6ab8fa969615457f
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-03-09 02:38:02 -0500

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/509A | Maximum in Table/250288007.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define rev(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define vec(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+ 
+using namespace std;
+ 
+void solve(){
+    int n;
+    cin>>n;
+    mat(int) grid(n, vector<int>(n));
+    f(i,0,n){
+        f(j,0,n){
+            if(i==0||j==0){
+                grid[i][j]=1;
+            } else {
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+            }
+        }
+    }
+    cout<<grid[n-1][n-1]<<'\n';
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    //cin>>t;
+    tc{
+        solve();
+    }
+ 
+    return 0;
+}
