8726e49839fc779b83a2c3c688531e977f57d5b4
Sujan Rokad <120612188+sujan-22@users.noreply.github.com>
2024-03-16 01:31:13 -0400

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1716A | 2-3 Moves/250470638.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define rev(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define vec(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+ 
+using namespace std;
+ 
+void solve(){
+    ll n;
+    cin>>n;
+    if(n==1){
+        cout<<2<<'\n';
+        return;
+    }
+    if(n%3==0){
+        cout<<n/3<<'\n';
+        return;
+    }
+    if((n-1)%3==0||(n+1)%3==0){
+        cout<<(n/3)+1<<'\n';
+        return;
+    }
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin>>t;
+    tc{
+        solve();
+    }
+ 
+    return 0;
+}
+ 
