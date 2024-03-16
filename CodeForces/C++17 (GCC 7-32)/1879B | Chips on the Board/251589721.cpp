72387161b648ae1ff482e43ddefe6526530cd4ff
Sujan Rokad <120612188+sujan-22@users.noreply.github.com>
2024-03-16 01:30:22 -0400

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1879B | Chips on the Board/251589721.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define rev(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define vt(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+ 
+using namespace std;
+ 
+void solve(){
+    ll n;
+    cin>>n;
+    vt(ll) a(n), b(n);
+    for(auto &i:a){
+        cin>>i;
+    }
+    for(auto &i:b){
+        cin>>i;
+    }
+    sort(all(a));
+    sort(all(b));
+    ll mn1=0, mn2=0;
+    f(i,0,n){
+        mn1+=a[0]+b[i];
+        mn2+=b[0]+a[i];
+    }
+    cout<<min(mn1,mn2)<<'\n';
+}
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin>>t;
+    tc{
+        solve();
+    }
+    return 0;
+}
