44fe3ffafb110a1f8973b2f5e3dd214ad0840001
Sujan Rokad <120612188+sujan-22@users.noreply.github.com>
2024-03-16 01:30:06 -0400

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1930A | Maximise The Score/251608673.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define rev(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define vt(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpair(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+#define pii pair<int, int>
+ 
+using namespace std;
+ 
+void solve(){
+    int n;
+    cin>>n;
+    vt(ll) a(2*n);
+    for(auto &i:a){
+        cin>>i;
+    }
+    sort(all(a));
+    ll ans=0LL;
+    for(int i=0; i<2*n-1; i+=2){
+        ans+=min(a[i],a[i+1]);
+    }
+    cout<<ans<<'\n';
+} 
+ 
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin>>t;
+    tc{
+        solve();
+    }
+    return 0;
+}
