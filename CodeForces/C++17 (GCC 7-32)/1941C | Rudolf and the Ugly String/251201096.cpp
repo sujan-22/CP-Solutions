6c84f161332967521a52eb5e71a899e225261f39
Sujan Rokad <120612188+sujan-22@users.noreply.github.com>
2024-03-16 01:30:37 -0400

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1941C | Rudolf and the Ugly String/251201096.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define rev(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define vt(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+ 
+using namespace std;
+ 
+void solve(){
+    int n; string s;
+    cin>>n>>s;
+    int c=0;
+    f(i,0,n-2){
+        if((s[i]=='m'&&s[i+1]=='a'&&s[i+2]=='p')||(s[i]=='p'&&s[i+1]=='i'&&s[i+2]=='e')){
+            //cout<<s[i]<<" "<<s[j]<<" "<<s[j+1]<<'\n';
+            ++c;
+            i+=2;
+        }
+    }
+    cout<<c<<'\n';
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin>>t;
+    tc{
+        solve();
+    }
+    return 0;
+}
