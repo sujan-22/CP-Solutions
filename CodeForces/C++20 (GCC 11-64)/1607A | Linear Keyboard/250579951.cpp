66860e61bcfa72c1e97a7407114d8ce602f2433a
Sujan Rokad <120612188+sujan-22@users.noreply.github.com>
2024-03-16 01:31:04 -0400

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1607A | Linear Keyboard/250579951.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i,p,n) for(int i=p; i<n; ++i)
+#define fr(i,p,n) for(int i=p; i>=n; --i)
+#define tc while(t--)
+#define all(x) (x).begin(), (x).end()
+#define rev(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define vec(T) vector<T>
+#define mat(T) vector<vector<T>>
+#define vpii(T) vector<pair<T, T>>
+#define uset(T) unordered_set<T>
+#define umap(T1, T2) unordered_map<T1, T2>
+#define pb push_back
+#define mp make_pair
+#define lcm(a, b) ((a) * (b)) / __gcd(a, b)
+#define count(n) floor(log10(n)+1)
+ 
+using namespace std;
+ 
+void solve(){
+    string s1,s2;
+    cin >> s1 >> s2;
+    umap(char, int) word;
+    f(i,0,26){
+        word[s1[i]] = i;
+    }
+    int result = 0;
+    f(i,1,s2.size()){
+        char prev = s2[i-1];
+        char cur = s2[i];
+        result += abs(word[cur] - word[prev]);
+    }
+    cout<<result<<'\n';
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    int t=1;
+    cin>>t;
+    tc{
+        solve();
+    }
+ 
+    return 0;
+}
+ 
